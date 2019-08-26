5644449ad6d611d530624cfa5ba27efde5baf1fb
Robert <robert0714@gmail.com>
2019-08-26 18:47:20 +0800

    Update pyspark01_r01.py

--- smack_code/ch06/samples_python/refactor01/pyspark01_r01.py
+++ smack_code/ch06/samples_python/refactor01/pyspark01_r01.py
@@ -2,7 +2,7 @@
 from pyspark.sql.dataframe import DataFrame 
 from refactor01.spark_config import PySpakCassandraConfig
 from refactor01.spark_config import PySpakMSSQLConfig
-
+import logging
 
 class Test(object):
 
@@ -20,12 +20,17 @@ class Test(object):
 
 
 if __name__ == "__main__":
-    print("開始執行RunWordCount")
-    test = Test()
-    users = test.load_and_get_table_df("mykeyspace", "users")
-    users.show()
+    logging.info("開始執行RunWordCount")
+    try:
+        test = Test()
+        users = test.load_and_get_table_df("mykeyspace", "users")
+        users.show()
     
-    # https://medium.com/coinmonks/running-pyspark-with-cassandra-using-spark-cassandra-connector-in-jupyter-notebook-9f1dc45e8dc9
-    print("下groupBy 與count 語法")
-    users.groupBy("user_id").count().orderBy('count', ascending=False).show()
+        # https://medium.com/coinmonks/running-pyspark-with-cassandra-using-spark-cassandra-connector-in-jupyter-notebook-9f1dc45e8dc9
+        print("下groupBy 與count 語法")
+        users.groupBy("user_id").count().orderBy('count', ascending=False).show()
+    except Exception as e:   
+        logging.error("%s", e, exc_info=False)
+    finally:
+        logging.info('end')
      
