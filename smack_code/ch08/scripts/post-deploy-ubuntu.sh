5ef98b2367af49910aaa3c0e96cbc02a51b8599f
robert.lee <robert0714@gmaill.com>
2019-09-10 23:04:05 +0800

    fixbug about zookeeper

--- smack_code/ch08/scripts/post-deploy-ubuntu.sh
+++ smack_code/ch08/scripts/post-deploy-ubuntu.sh
@@ -4,12 +4,14 @@ value=$( grep -ic "entry" /etc/hosts )
 if [ $value -eq 0 ]
 then
 echo "
+127.0.0.1       localhost
+::1             localhost
 ################ cassandra-cookbook host entry ############
 107.170.38.238  MACHINE01
 107.170.112.81  MACHINE02
 107.170.115.161 MACHINE03
 ######################################################
-" >> /etc/hosts
+" > /etc/hosts
 fi
 # sudo apt-get update
 sudo apt-get  install -y rsync openjdk-8-jdk  nfs-common portmap  
@@ -45,7 +47,21 @@ sudo chown -R hduser:hadoop scala
 sudo sh -c 'echo export SCALA_HOME=/usr/local/scala >> /home/hduser/.bashrc'
 sudo sh -c 'echo export PATH=\$PATH:\$SCALA_HOME/bin >> /home/hduser/.bashrc'
 
+# Download zookeeper to the vagrant shared directory if it doesn't exist yet
+cd /vagrant
+if [ ! -f apache-zookeeper-3.5.5-bin.tar.gz ]; then
+	wget https://archive.apache.org/dist/zookeeper/zookeeper-3.5.5/apache-zookeeper-3.5.5-bin.tar.gz
+fi
+# Unpack zookeeper and install
+sudo tar vxzf  apache-zookeeper-3.5.5-bin.tar.gz  -C /usr/local
+cd /usr/local
+sudo mv apache-zookeeper-3.5.5-bin  zookeeper
+cp  /usr/local/zookeeper/conf/zoo_sample.cfg  /usr/local/zookeeper/conf/zoo.cfg
+sudo chown -R hduser:hadoop zookeeper
 
+# zookeeper variables
+sudo sh -c 'echo export ZOOKEEPER_HOME=/usr/local/zookeeper >> /home/hduser/.bashrc'
+sudo sh -c 'echo export PATH=\$PATH:\$ZOOKEEPER_HOME/bin >> /home/hduser/.bashrc'
 
 # Download Kafka to the vagrant shared directory if it doesn't exist yet
 # https://archive.apache.org/dist/kafka/0.10.2.2/kafka_2.12-0.10.2.2.tgz
@@ -75,4 +91,4 @@ sudo chown -R hduser:hadoop /home/hduser/kafka-samples
 
 sudo curl https://bintray.com/sbt/rpm/rpm > bintray-sbt-rpm.repo
 sudo mv bintray-sbt-rpm.repo /etc/yum.repos.d/
-sudo apt-get install -y sbt screen
+sudo apt-get install -y sbt screen   lsof
